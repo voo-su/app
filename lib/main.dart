9d09ca2cc1ca677f96986bdd00f238f19b9c94af
Magomedcoder <info@magomedcoder.ru>
2025-01-06 20:21:45 +0300

    Chore: added submodule with proto files and generated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
-void main() {
+import 'package:grpc/grpc.dart';
+import 'package:voo_su/data/data_sources/remote/grpc/gen/dart/pb/auth.pbgrpc.dart';
 
+Future<void> main() async {
+  final channel = ClientChannel(
+    '192.168.0.2',
+    port: 50051,
+    options: const ChannelOptions(
+      credentials: ChannelCredentials.insecure(),
+    ),
+  );
+
+  final client  = AuthServiceClient(channel);
+
+  final request = AuthLoginRequest(
+    email: "info@magomedcoder.ru",
+    platform: "android",
+  );
+
+  final login = await client.login(request);
+print(login.token);
 }
